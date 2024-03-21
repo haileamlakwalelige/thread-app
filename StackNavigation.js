caf852bd02a7fcd6c2ecfa253dee1c67092e2538
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-22 00:15:03 +0300

    update the stack navigation and update data to the .env file

--- StackNavigation.js
+++ StackNavigation.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-
-
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const StackNavigation = () => {
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
-    return (
-        <NavigationContainer>
-          <Stack.Navigator>
-          <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
-          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
-         
-          </Stack.Navigator>
-        </NavigationContainer>
-      );
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default StackNavigation;
-
