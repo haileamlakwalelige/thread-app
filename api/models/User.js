211bc513e43a4d9caf6be8a45524075389b5dcaf
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-22 00:08:47 +0300

    update user models

--- api/models/User.js
+++ api/models/User.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    unique: true,
+
     required: true,
   },
   email: {
     type: String,
+    unique: true,
     required: true,
   },
   password: {
